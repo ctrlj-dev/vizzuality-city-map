7e496b5ab43f05114dba6c063dd7e08b8893c349
ctrlj-dev <jesuscc1701@gmail.com>
2024-12-09 13:42:11 +0100

    feat(networks): add network view foundation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,5 @@
-'use client';
-
-import Link from 'next/link';
-import { useState } from 'react';
+import { NetworkView } from '@/components/networks';
 
 export default function Home() {
-  const [id, setId] = useState('');
-
-  const handleId = (value: string) => {
-    if (!value) {
-      return;
-    }
-    setId(value);
-  };
-
-  return (
-    <div>
-      <input
-        placeholder="introduce id"
-        onChange={event => handleId(event.target.value)}
-      />
-      <Link href={`networks/${id}`} shallow>
-        Details
-      </Link>
-    </div>
-  );
+  return <NetworkView />;
 }
---
+++ src/components/networks/index.ts
@@ -0,0 +1,1 @@
+export { default as NetworkView } from './NetworksView';
