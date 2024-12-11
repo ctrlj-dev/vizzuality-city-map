2abb608578d7e638c333082240523a6d1c06452f
ctrlj-dev <jesuscc1701@gmail.com>
2024-12-11 22:05:27 +0100

    feat(search): add styles

--- src/components/ui/Search/Search.tsx
+++ src/components/ui/Search/Search.tsx
@@ -1,13 +1,19 @@
-// SearchBar.tsx
-import { cn } from '@/lib/utils'; // Asegúrate de tener esta función para concatenar clases
-import { Search as SearchIcon } from 'lucide-react'; // Importa el ícono de la biblioteca lucide
+'use client';
+
+import { cn } from '@/lib/utils';
+import { Search as SearchIcon } from 'lucide-react';
 import React from 'react';
+import { Input } from '../Input';
 
 interface SearchBarProps extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
 }
 
-const Search = ({ placeholder, className, ...props }: SearchBarProps) => {
+const Search = ({
+  placeholder = 'Search...',
+  className,
+  ...props
+}: SearchBarProps) => {
   return (
     <div className="relative w-full">
       <SearchIcon
@@ -15,11 +21,11 @@ const Search = ({ placeholder, className, ...props }: SearchBarProps) => {
         className="absolute left-4 top-1/2 transform -translate-y-1/2 text-primary-800"
         size={24}
       />
-      <input
+      <Input
         type="text"
-        placeholder={placeholder || 'Search...'}
+        placeholder={placeholder}
         className={cn(
-          'w-full rounded-full pl-12 pr-4 py-3 border border-primary-200 text-primary-800 font-medium focus-visible:outline-none focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2 focus-within:ring-primary-800 placeholder:text-primary-800 disabled:cursor-not-allowed disabled:opacity-50',
+          'w-full h-full rounded-full pl-12 pr-4 py-3 border border-primary-200 text-primary-800 font-medium focus-visible:outline-none focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2 focus-within:ring-primary-800 placeholder:text-primary-800 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         {...props}
