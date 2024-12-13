3630e6ec120046c67510366f771f48dcd8db28b6
ctrlj-dev <jesuscc1701@gmail.com>
2024-12-13 01:41:25 +0100

    feat(page): remove params

--- src/app/networks/[id]/page.tsx
+++ src/app/networks/[id]/page.tsx
@@ -1,15 +1,7 @@
-interface DetailPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function DetailPage({ params }: DetailPageProps) {
-  const { id } = params;
+export default async function DetailPage() {
   return (
     <div>
       <h1>Detail page</h1>
-      <p>{id}</p>
     </div>
   );
 }
